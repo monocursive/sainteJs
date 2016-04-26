fd39d25695f5a5c747babe5474044d379cf7eb25
maz-dev <contact@maz-dev.cc>
2016-04-27 00:06:58 +0200

    Geocode event location

--- imports/api/events/methods.js
+++ imports/api/events/methods.js
@@ -2,14 +2,17 @@ import {Meteor} from 'meteor/meteor';
 import {SimpleSchema} from 'meteor/aldeed:simple-schema';
 import {Events} from './events';
 
+const geocoderProvider = 'google';
+const httpAdapter = 'http';
+const geocoder = require('node-geocoder')(geocoderProvider, httpAdapter);
+
 export default function() {
   Meteor.methods({
-    'events.add'({title, speaker, description, venue, date}) {
+    'events.add'({title, speaker, description, venue, adress, date}) {
       let loggedInUser = Meteor.user();
 
-      if (!loggedInUser ||
-        !Roles.userIsInRole(loggedInUser, 'admin')) {
-        throw new Meteor.Error(403, "Access denied");
+      if (!loggedInUser || !Roles.userIsInRole(loggedInUser, 'admin')) {
+        throw new Meteor.Error(403, "Accès refusé");
       }
 
       new SimpleSchema({
@@ -17,11 +20,15 @@ export default function() {
         speaker: {type: String},
         description: {type: String},
         venue: {type: String},
+        adress: {type: String},
         date: {type: Date}
-      }).validate({title, speaker, description, venue, date});
+      }).validate({title, speaker, description, venue, adress, date});
+
+      geocoder.geocode(adress, Meteor.bindEnvironment(function(err, res) {
+        Events.insert({title, speaker, description, venue, adress, res, date});
+      }));
 
 
-      Events.insert({title, speaker, description, venue, date});
     }
   });
 }
