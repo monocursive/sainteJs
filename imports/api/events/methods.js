af517ff43c9cfe3f76f36dd27fa04079cdd3ac89
maz-dev <contact@maz-dev.cc>
2016-04-27 10:40:23 +0200

    Unblock events add method

--- imports/api/events/methods.js
+++ imports/api/events/methods.js
@@ -6,29 +6,29 @@ const geocoderProvider = 'google';
 const httpAdapter = 'http';
 const geocoder = require('node-geocoder')(geocoderProvider, httpAdapter);
 
-export default function() {
-  Meteor.methods({
-    'events.add'({title, speaker, description, venue, adress, date}) {
-      let loggedInUser = Meteor.user();
+Meteor.methods({
+  'events.add'({title, speaker, description, venue, adress, date}) {
+    this.unblock();
+    
+    let loggedInUser = Meteor.user();
 
-      if (!loggedInUser || !Roles.userIsInRole(loggedInUser, 'admin')) {
-        throw new Meteor.Error(403, "Accès refusé");
-      }
-
-      new SimpleSchema({
-        title: {type: String},
-        speaker: {type: String},
-        description: {type: String},
-        venue: {type: String},
-        adress: {type: String},
-        date: {type: Date}
-      }).validate({title, speaker, description, venue, adress, date});
-
-      geocoder.geocode(adress, Meteor.bindEnvironment(function(err, res) {
-        Events.insert({title, speaker, description, venue, adress, res, date});
-      }));
-
-
+    if (!loggedInUser || !Roles.userIsInRole(loggedInUser, 'admin')) {
+      throw new Meteor.Error(403, "Accès refusé");
     }
-  });
-}
+
+    new SimpleSchema({
+      title: {type: String},
+      speaker: {type: String},
+      description: {type: String},
+      venue: {type: String},
+      adress: {type: String},
+      date: {type: Date}
+    }).validate({title, speaker, description, venue, adress, date});
+
+    geocoder.geocode(adress, Meteor.bindEnvironment(function(err, res) {
+      Events.insert({title, speaker, description, venue, adress, res, date});
+    }));
+
+
+  }
+});
