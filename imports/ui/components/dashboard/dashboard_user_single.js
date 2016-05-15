cb357b19195bf55c03770b3078d5477080b4c9e2
maz-dev <contact@maz-dev.cc>
2016-05-15 20:07:33 +0200

    Check if username only in dashboard users screen

--- imports/ui/components/dashboard/dashboard_user_single.js
+++ imports/ui/components/dashboard/dashboard_user_single.js
@@ -4,7 +4,7 @@ export default class DashboardUserSingle extends Component {
   render() {
     return(
       <tr>
-        <td>{this.props.user.emails[0].address}</td>
+        <td>{'emails' in this.props.user ? this.props.user.emails[0].address : "Username only"}</td>
         <td>{this.props.user.username}</td>
         <td>{this.props.user.profile.notif_email ? "oui" : "non"}</td>
         <td>Button</td>
